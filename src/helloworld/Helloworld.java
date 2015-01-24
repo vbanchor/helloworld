6564d4b7f2e7d6665ec0df92d4f9e318081b7321
vbanchor <akbacka@gmail.com>
2015-01-23 17:10:08 -0900

    java hello world project

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author Expression Victoria_Backa is undefined on line 12, column 14 in Templates/Classes/Main.java.
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello everybody!");
+    }
+}
+
